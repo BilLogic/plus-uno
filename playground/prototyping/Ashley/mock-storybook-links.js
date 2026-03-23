85de5c632b65cb6d9ba666242f0ffaded191b10d
weiyinx-axu <weiyinx@andrew.cmu.edu>
2026-03-23 14:41:21 -0700

    cleanup: remove temporary sed backup files

--- playground/prototyping/Ashley/mock-storybook-links.js
+++
@@ -1,5 +0,0 @@
-
-export const linkTo = (storyId, storyName) => () => {
-    console.log(`[Mock] Link to story: ${storyId} - ${storyName}`);
-    alert(`Preview: Navigation to ${storyId} is not available in standalone mode.`);
-};
